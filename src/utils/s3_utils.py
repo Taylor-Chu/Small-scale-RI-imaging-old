ae795c82fc08f6ef99a54aac065757bb96dc2a6c
Taylor HWU clsuter <cc2040@hw.ac.uk>
2026-03-13 17:29:34 +0000

    updated s3 function

--- src/utils/s3_utils.py
+++ src/utils/s3_utils.py
@@ -6,16 +6,6 @@ import glob
 import fsspec
 from io import BytesIO
 
-bucket_name = "r2d2data"
-
-config = Config(
-    request_checksum_calculation="when_required",
-    response_checksum_validation="when_required",
-)
-s3 = boto3.resource('s3', config=config)
-bucket = s3.Bucket(bucket_name)
-
-fs = fsspec.filesystem("s3")
 
 def download_file_from_s3(s3_key: str, local_path: str):
     """
@@ -25,4 +15,18 @@ def download_file_from_s3(s3_key: str, local_path: str):
         s3_key (str): The S3 key of the file to download.
         local_path (str): The local path where the file will be saved.
     """
-    bucket.download_file(s3_key, local_path)
+    os.environ["AWS_ACCESS_KEY_ID"] = "Y7MG0DGL3A2LY1ANN5BZ"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "GMCWK7aAn1ZlAIcFf8h2tYn7D2RDYdzjlxWkSYde"
+    os.environ["AWS_ENDPOINT_URL"] = "https://s3.eidf.ac.uk"
+    bucket_name = "r2d2data"
+
+    config = Config(
+        request_checksum_calculation="when_required",
+        response_checksum_validation="when_required",
+    )
+    s3 = boto3.resource('s3', config=config)
+    bucket = s3.Bucket(bucket_name)
+
+    fs = fsspec.filesystem("s3")
+    fs.get(s3_key, local_path)
+    print(f"Downloaded {s3_key} to {local_path}")
